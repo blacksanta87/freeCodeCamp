a075e645b6494f3125967ef337b97e9a07823286
blacksanta87 <ahmadabbaszainol@gmail.com>
2019-03-04 21:24:51 +0800

    constant for read-only variable

---
+++ es6/constantVariable.js
@@ -0,0 +1,9 @@
+function printManyTimes(str) {
+  'use strict';
+  const sentence = str + ' is cool!';
+  for (let i = 0; i < str.length; i += 2) {
+    console.log(sentence);
+  }
+}
+
+printManyTimes('freeCodeCamp');
