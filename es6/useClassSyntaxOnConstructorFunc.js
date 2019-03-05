5b42c6cc2b9d668e429761650ca4a826facc7742
blacksanta87 <ahmadabbaszainol@gmail.com>
2019-03-05 16:01:35 +0800

    use class syntax on constructor function

---
+++ es6/useClassSyntaxOnConstructorFunc.js
@@ -0,0 +1,13 @@
+function makeClass() {
+  'use strict';
+  class Vegetable {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+  return Vegetable;
+}
+
+const Vegetable = makeClass();
+const carrot = new Vegetable('carrot');
+console.log(carrot.name);
