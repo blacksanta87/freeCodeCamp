5eb2723ec27b07559ff9d67d6d4f14bd2b3cedee
blacksanta87 <ahmadabbaszainol@gmail.com>
2019-03-06 20:06:19 +0800

    using test method on string

---
+++ RegExp/ex1.js
@@ -0,0 +1,8 @@
+// Using test method.
+// test will take the regex and apply it to string.
+// return either true or false
+let myString = 'Hello World!';
+let myRegex = /Hello/;
+let result = myRegex.test(myString);
+
+console.log(result);
