cbc9c2f9b6000f632ca7f437260ae9944eca04b5
blacksanta87 <ahmadabbaszainol@gmail.com>
2019-03-08 09:00:02 +0800

    use console.clear() to clear console

---
+++ debugging/ex2.js
@@ -0,0 +1,7 @@
+// understanding difference between freeCodeCamp and Browser console.
+let outputTwo = 'This will print to the browser console 2 times' // only on freeCodeCamp site.
+console.log(outputTwo);
+
+let outputOne = 'Try to get this to log only once to the browser console';
+console.clear();
+console.log(outputOne);
