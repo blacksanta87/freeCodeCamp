4488413760c7eb9143a97a7872132c8468fbb5e8
blacksanta87 <ahmadabbaszainol@gmail.com>
2019-03-08 21:46:36 +0800

    use for loop to iterate array

---
+++ basicDataStructure/ex11.js
@@ -0,0 +1,12 @@
+// iterate through all an array's items using for loops
+function filteredArray(arr, elem) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i].indexOf(elem) === -1) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+console.log(filteredArray( [[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3) );
