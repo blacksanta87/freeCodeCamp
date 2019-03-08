fc556721b34ec58eabfb44b05c2c3ffc817ee95f
blacksanta87 <ahmadabbaszainol@gmail.com>
2019-03-08 09:05:34 +0800

    correct variable misspelled

---
+++ debugging/ex4.js
@@ -0,0 +1,5 @@
+// catch misspelled variable and function names
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+console.log(`Net working capital is: ${netWorkingCapital}`);
