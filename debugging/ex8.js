9b0fba5f2e2788aabf0341d3b6dcfb55d4383e93
blacksanta87 <ahmadabbaszainol@gmail.com>
2019-03-08 09:14:55 +0800

    fix function call

---
+++ debugging/ex8.js
@@ -0,0 +1,9 @@
+// catch missing open and closing parenthesis after a function call
+function getNine() {
+  let x = 6;
+  let y = 3;
+  return x + y;
+}
+
+let result = getNine();
+console.log(result);
