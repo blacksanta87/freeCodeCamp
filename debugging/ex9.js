d395943023bda2e5a015f13b50919692d0f58491
blacksanta87 <ahmadabbaszainol@gmail.com>
2019-03-08 09:16:51 +0800

    fix argument order

---
+++ debugging/ex9.js
@@ -0,0 +1,9 @@
+// catch argument passed in the wrong order when calling a function
+function raiseToPower(b, e) {
+  return Math.pow(b, e);
+}
+
+let base = 2;
+let exp = 3;
+let power = raiseToPower(base, exp);
+console.log(power);
