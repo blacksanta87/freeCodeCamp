93ce1836ed647af91b8b4e8b507f0c96bf30b958
blacksanta87 <ahmadabbaszainol@gmail.com>
2019-03-08 09:13:20 +0800

    make correction on assignment operator

---
+++ debugging/ex7.js
@@ -0,0 +1,12 @@
+// catch assignment operator instead of equality operator.
+let x = 7;
+let y = 9;
+let result = "to come";
+
+if(x === y) {
+  result = "Equal!";
+} else {
+  result = "Not equal!";
+}
+
+console.log(result);
