a370792e414bb2f8b163a4673c9d05f74d07069a
blacksanta87 <ahmadabbaszainol@gmail.com>
2019-03-11 08:13:55 +0800

    finish title case str

--- basicAlgorithm/ex11.js
+++ basicAlgorithm/ex11.js
@@ -1,6 +1,24 @@
 // Title case a sentence
 function titleCase(str) {
+  // return first letter capitalize for each words.
+  // the rest are lower case.
+  // capitalize the connecting words to. eg: the, of
+  let splitStr = str.split(' ');
+  let result = [];
   
+  for(let i = 0; i < splitStr.length; i++) {
+    let subSplitStr = splitStr[i].split('');
+    subSplitStr[0] = subSplitStr[0].toUpperCase();
+    
+    for(let j = 1; j < subSplitStr.length; j++) {
+      subSplitStr[j] = subSplitStr[j].toLowerCase();
+    }
+
+    let joinedStr = subSplitStr.join('');
+    result.push(joinedStr);
+  }
+  return result.join(' ');
 }
 
+console.log(titleCase("SO, YOU WANT TO DANCE?"));
 console.log(titleCase("I'm a little tea pot"));
