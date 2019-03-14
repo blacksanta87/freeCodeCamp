944695d4dd6f9f4bd36ed6bf5194cca77a31ae75
blacksanta87 <ahmadabbaszainol@gmail.com>
2019-03-14 14:40:27 +0800

    Create src directory

--- learningJS/jsCodecademy/index.js
+++
@@ -1,4 +0,0 @@
-var pasta = 'Spaghetti';
-const meat = 'Pancetta';
-let sauce = 'Eggs and cheese';
-const carnonara = `You can make carbonara with ${pasta}, ${meat}, and a sauce made with ${sauce}.`;
